074db419a087fe88ea5c5c802d4c5e714296f7b7
ugurb <ugurbasaran3@gmail.com>
2020-03-31 21:28:11 +0300

    Configured for connecting to postgresql on aws

--- HangFire.Scheduler/Startup.cs
+++ HangFire.Scheduler/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.PostgreSql;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,10 +28,20 @@ namespace HangFire.Scheduler
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddHangfire(configuration => configuration
+                                                    .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                                                    .UseSimpleAssemblyNameTypeSerializer()
+                                                    .UseRecommendedSerializerSettings()
+                                                    .UsePostgreSqlStorage(Configuration.GetConnectionString("HangfireConnection"))
+                                 );
+
+            // Add the processing server as IHostedService
+            services.AddHangfireServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IBackgroundJobClient backgroundJobs, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,6 +58,8 @@ namespace HangFire.Scheduler
             {
                 endpoints.MapControllers();
             });
+
+            app.UseHangfireDashboard();
         }
     }
 }
